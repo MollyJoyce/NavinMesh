9c9040662dae3144ca98f5bdefa6277af8081b7d
Molly Joyce <mjoyce8695@student.pps.net>
2021-11-18 13:25:04 -0800

    Navin' that mesh

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    bool open = false;
+
+    void Update(){
+        if(Input.GetKeyDown("space")){
+            if(open == true){
+            this.transform.Translate(0,-3,0);
+            
+            }else{
+            this.transform.Translate(0,3,0);
+            
+        }
+
+            open = !open;
+        }
+    }
+}
